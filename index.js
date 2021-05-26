1a262e8680e2f7b57c978fc9eec80adb881e03a3
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2021-05-26 12:40:28 +0530

    When new user arrives in server their details such as -- UserId,UserName,UserTag,UserAvathar --  are saved in mongodb database

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 //All dependencies are imported here
 const Discord = require("discord.js");
 const config = require("./config.json");
-const mongoose = require('mongoose');
-const {MongoClient} = require("mongodb");
+const {
+    MongoClient
+} = require("mongodb");
 
 //declaring the instance 
 const client = new Discord.Client();
@@ -10,53 +11,65 @@ PREFIX = config.prefix;
 const clientMongo = new MongoClient(config.uri);
 
 
+
+
 //------------------Main code goes here-----------------------
-
-//Mongodb connection 
-// mongoose.connect(config.uri, {useNewUrlParser: true, useUnifiedTopology: true});
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function() {
-//   console.log("Connection success");
-// });
-async function run() {
-    try {
-        // Connect the client to the server
-        await clientMongo.connect();
-
-        const db = clientMongo.db('Data')
-        const userCollection = db.collection('userDetails')
-
-        details = {
-            userId: 1234567890,
-            userUsername: "qwertyuiop",
-            userTag: "sgfdh#2435",
-            userAvatar: "https://cdn.discordapp.com/avatars/658331305104375830/0aea486923618ef9358ebfbfa661f51a.webp"
-        };
-        const result = await userCollection.insertOne(details);
-        console.log(
-
-            `${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`,
-
-        );
-
-        // Establish and verify connection
-        console.log("Connected successfully to server");
-        console.log(quotesCollection);
-    } catch {
-        console.error();
-    }
-}
-run().catch(console.dir);
-
 
 
 
 //Welcome
 client.on('guildMemberAdd', member => {
     const channel = member.guild.channels.cache.find(ch => ch.id === '736874356738752585');
-    if (!channel) console.log("channel bokuno pick");;
+    if (!channel) console.log("channel bokuno pico");
+
     channel.send(`Welcome to the server, ${member}`);
+    channel.send(`${member.id}`);
+    channel.send(`${member.user.username}`);
+    channel.send(`${member.user.tag}`);
+    channel.send(`${member.user.displayAvatarURL()}`);
+
+
+
+
+
+    //----------------Uploading New User Details To Database----------------------
+
+    //Mongodb connection 
+
+    async function run() {
+        try {
+            // Connect the client to the server
+            await clientMongo.connect();
+
+            const db = clientMongo.db('Data')
+            const userCollection = db.collection('userDetails')
+
+            details = {
+                userId: member.id,
+                userUsername: member.user.username,
+                userTag: member.user.tag,
+                userAvatar: member.user.displayAvatarURL()
+            };
+            const result = await userCollection.insertOne(details);
+            console.log(
+
+                `${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`,
+
+            );
+
+            // Establish and verify connection
+            console.log("Connected successfully to server");
+            console.log(quotesCollection);
+        } catch {
+            console.error();
+        }
+    }
+    run().catch(console.dir);
+
+
+
+
+
 });
 
 
@@ -81,7 +94,26 @@ client.on('message', msg => {
 //---------test message-------------
 client.on('message', msg => {
     if (msg.content === `${PREFIX}test`) {
-        msg.channel.send(`Your test output: ${msg.author.lastMessageID}!`);
+        //getting user details in that servver
+        // const list = client.guilds.cache.get("736874356168327182");
+        // console.log(list);
+        // list.forEach(id => console.log(id));
+
+        //list.members.forEach(member => console.log(member.user.username));
+        // msg.channel.send(`Your test output: ${JSON.stringify(list)}!`);
+
+        // newMemberId = client.fetchUser(member.id);
+        // channel.send(`tag: ${newMemberId.tag}`);
+
+        const Guild = client.guilds.cache.get("736874356168327182"); // Getting the guild.
+        const MemberId = Guild.members.cache.map(member => member.id); // Getting the members and mapping them by ID.
+        const MemberName = Guild.members.cache.map(member => member.user.username);
+        const MemberTag = Guild.members.cache.map(member => member.user.tag);
+        const MemberAvathar = Guild.members.cache.map(member => member.user.displayAvatarURL());
+        //console.log(`${MemberTag} '\n' ${MemberAvathar} '**'`);
+        msg.channel.send(`${MemberId}'\n'  ${MemberName} '\n' ${MemberTag}`);
+        msg.channel.send(`'\n' ${MemberAvathar}`);
+
     }
 });
 
